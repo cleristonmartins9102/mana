63a0383075013b99a54044a90103b05993bc48d3
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-04-22 23:20:02 -0300

    feat: add new fields to createAccountDTO model

--- src/main/java/com/mana/features/CreateAccountUseCase.java
+++ src/main/java/com/mana/features/CreateAccountUseCase.java
@@ -4,6 +4,7 @@ import com.mana.domain.CreateAccount;
 import com.mana.domain.Encrypt;
 import com.mana.domain.Hash;
 import com.mana.domain.models.Account;
+import com.mana.domain.models.CreateAccountDTO;
 import com.mana.domain.models.TokenPayload;
 import com.mana.features.contract.SaveAccountRepository;
 
@@ -19,9 +20,9 @@ public class CreateAccountUseCase implements CreateAccount{
   }
 
   @Override
-  public Account create(String firstName, String secondName, String email, String password) {
-    final String hashPassword = this.bcryptAdapter.hash(password);
-    final TokenPayload payload = new TokenPayload(firstName, secondName, email);
+  public Account create(CreateAccountDTO createAccountData) {
+    final String hashPassword = this.bcryptAdapter.hash(createAccountData.password());
+    final TokenPayload payload = new TokenPayload(createAccountData., secondName, email);
     final String token = this.jsonWebTokenAdapter.encrypt(payload);
     final String accountId = this.accountRepository.save(firstName);
     return null;
