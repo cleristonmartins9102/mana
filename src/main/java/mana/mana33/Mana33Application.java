a89e4380ad76b1c939f7023600ebb894466905bc
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-05-07 23:51:53 +0100

    feat: add spring scafolder

---
+++ src/main/java/mana/mana33/Mana33Application.java
@@ -0,0 +1,12 @@
+package mana.mana33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mana33Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Mana33Application.class, args);
+    }
+
+}
---
+++ src/test/java/mana/mana33/Mana33ApplicationTests.java
@@ -0,0 +1,13 @@
+package mana.mana33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mana33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
