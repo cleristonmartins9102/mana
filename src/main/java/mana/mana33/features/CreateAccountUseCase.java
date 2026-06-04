c890079cf112de48d19dab87999a43fa20b0cf18
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-06-04 06:58:26 -0300

    feat: implement DI of SaveUserRepository in CreateAccountUseCase

--- src/main/java/mana/mana33/features/CreateAccountUseCase.java
+++ src/main/java/mana/mana33/features/CreateAccountUseCase.java
@@ -3,16 +3,20 @@ package mana.mana33.features;
 import mana.mana33.domain.CreateAccount;
 import mana.mana33.domain.Encrypt;
 import mana.mana33.domain.Hash;
+import mana.mana33.domain.SaveUserRepository;
 import mana.mana33.domain.models.CreateAccountDTO;
+import mana.mana33.domain.models.SaveUserModel;
 import mana.mana33.domain.models.TokenPayloadDTO;
 
 public class CreateAccountUseCase implements CreateAccount {
     private final Hash hasher;
     private final Encrypt encrypter;
+    private final SaveUserRepository saveUserRepository;
 
-    public  CreateAccountUseCase(Hash hasher, Encrypt encrypter) {
+    public  CreateAccountUseCase(Hash hasher, Encrypt encrypter, SaveUserRepository saveUserRepository) {
         this.hasher = hasher;
         this.encrypter = encrypter;
+        this.saveUserRepository = saveUserRepository;
     }
 
     @Override
@@ -25,5 +29,16 @@ public class CreateAccountUseCase implements CreateAccount {
         payload.email = createAccountDTO.email();
 
         String token = this.encrypter.encrypt(payload);
+
+        SaveUserModel saveUserModel = new SaveUserModel(
+                createAccountDTO.firstName(),
+                createAccountDTO.secondName(),
+                createAccountDTO.email(),
+                hashedPassword,
+                createAccountDTO.mobileNumber(),
+                token
+        );
+
+        this.saveUserRepository.save(saveUserModel);
     }
 }
