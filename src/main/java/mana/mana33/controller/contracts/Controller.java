5b85cb22ccced67d25c6580273be2e331cdcf7a8
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-06-16 07:08:19 -0300

    feat: add getValidators method with default empty implementation
    - Add getValidators method returning Validate<IB>
    - Provide default empty validator (no-op)
    - Allow controllers to override and define custom validation logic

--- src/main/java/mana/mana33/controller/contracts/Controller.java
+++ src/main/java/mana/mana33/controller/contracts/Controller.java
@@ -2,9 +2,14 @@ package mana.mana33.controller.contracts;
 
 import mana.mana33.controller.http.HttpRequest;
 import mana.mana33.controller.http.HttpResponse;
+import mana.mana33.features.contracts.Validate;
 
 public abstract class Controller<R, IB, IQ> {
     public abstract HttpResponse<R> perform(HttpRequest<IB, IQ> input);
+
+    public Validate<IB> getValidators() {
+        return input -> {};
+    }
 
     public HttpResponse<R> handler(HttpRequest<IB, IQ> input) {
         return this.perform(input);
