4627b6e83eaaf947affe67115e690875eb9b9a11
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-06-18 08:18:42 -0300

    feat: add REST endpoint to CreateAccountController

--- src/main/java/mana/mana33/controller/CreateAccountController.java
+++ src/main/java/mana/mana33/controller/CreateAccountController.java
@@ -1,12 +1,25 @@
-package mana33.controller;
+package mana.mana33.controller;
 
 import mana.mana33.domain.models.CreateAccountDTO;
 import mana.mana33.controller.contracts.Controller;
 import mana.mana33.controller.http.HttpRequest;
 import mana.mana33.controller.http.HttpResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/accounts")
 public class CreateAccountController
         extends Controller<String, CreateAccountDTO, Void> {
+
+    @PostMapping
+    public HttpResponse<String> createAccount(@RequestBody CreateAccountDTO body) {
+        HttpRequest<CreateAccountDTO, Void> request = new HttpRequest<>();
+        request.body = body;
+        return this.handler(request);
+    }
 
     @Override
     public HttpResponse<String> perform(
